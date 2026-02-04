a4296718f084d701dbcb8be9f64db000f090595b
ValeJade <jadevale.dev@gmail.com>
2026-02-04 12:19:53 -0300

    implementa classificador de nível de herói

--- myFirstOne.js
+++ myFirstOne.js
@@ -1,1 +1,25 @@
-console.log("iaí mona?")
+let nome = "Jade";
+let xp = 10001;
+let nivel = "5000";
+ if (xp <1000){
+    nivel = "ferro";
+    } else if (xp <= 2000){
+        nivel = "bronze";
+     } else if (xp <= 5000){
+        nivel = "prata";
+     }else if ( xp <= 7000){
+        nivel = "ouro";
+     }else if ( xp <= 8000){
+        nivel= "platina";
+     }else if ( xp <= 9000){
+            nivel= "ascendente";
+     }else if (xp <=10000){
+        nivel= "imortal";
+     }else if (xp > 10000){
+            nivel= "radiante";
+
+     }
+        
+
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
